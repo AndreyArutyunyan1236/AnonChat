17198364875e3b9d8e6cf87a14bc8898c61939d7
Andrey <kaspersky1236@gmail.com>
2025-08-01 22:34:35 +0400

    AnonChat's minimal functionality is done

--- main/views.py
+++ main/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Message
+from .forms import MessageForm 
 
-# Create your views here.
+def chat(request):
+    
+    messages = Message.objects.all()
+    messageForm = MessageForm()
+    
+    if request.method == 'POST':
+        messageForm = MessageForm(request.POST)
+        if messageForm.is_valid():
+            messageForm.save()
+            return redirect('Chat')
+    
+    context = {
+        'messages': messages,
+        'form': messageForm,
+    }
+    
+    return render(request, 'main/index.html', context)
+
+
+# adminkan el kanes
